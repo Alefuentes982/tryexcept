7fd63615a55e9ee2f93f7580bd3fa5aa3ae3d082
AleFuentes982 <jano231982@gmail.com>
2021-10-09 23:56:01 -0300

    first commit

---
+++ tryexcept.py
@@ -0,0 +1,7 @@
+try:
+    num1 = 3
+    num2 = 1
+    print(num1/num2)
+    print("Calculo realizado correctamente")
+except ZeroDivisionError:
+    print("Un Error a ocurrido al tratar de dividir entre 0")
